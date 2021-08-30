094e66184e0bc839836d574d1bf28ac640c29c21
joseph tran <anhtt@a-livesystems.com>
2021-08-30 16:27:35 +0700

    Mutation User

---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,53 @@
+import { User } from "../entities/User";
+import { Arg, Mutation, Resolver } from "type-graphql";
+import argon2d from "argon2";
+import { UserMutationResponse } from "../types/UserMutationResponse";
+
+@Resolver()
+export class UserResolver {
+  @Mutation(_returns => UserMutationResponse, { nullable: true })
+  async register(
+    @Arg('email') email: string,
+    @Arg('username') username: string,
+    @Arg('password') password: string
+  ): Promise<UserMutationResponse> {
+    try {
+      const existingUSer = await User.findOne({
+        where: [{ username }, { email }]
+      })
+      if (existingUSer) return {
+        code: 400,
+        success: false,
+        message: 'Duplicated username or email',
+        errors: [
+          {
+            field: existingUSer.username === username ? 'username' : 'email',
+            message: `${existingUSer.username === username ? 'Username' : 'Email'} already taken`
+          }
+        ]
+      };
+
+      const hashedPassword = await argon2d.hash(password)
+
+      const newUser = User.create({
+        username,
+        password: hashedPassword,
+        email
+      })
+
+      return {
+        code: 200,
+        success: true,
+        message: 'User registration successful',
+        user: await User.save(newUser)
+      }
+    } catch (error) {
+      console.log(error);
+      return {
+        code: 500,
+        success: false,
+        message: `Internal server error ${error.message}`,
+      };
+    }
+  }
+}
